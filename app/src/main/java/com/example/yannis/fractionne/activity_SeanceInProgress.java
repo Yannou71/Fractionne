1f69b424ca9df7cdc1919e38c795938e60c12573
yannou71 <yannis.banos@insa-lyon.fr>
2019-03-21 18:04:25 +0100

    Création de l'activity : SeanceIn Progress

---
+++ app/src/main/java/com/example/yannis/fractionne/activity_SeanceInProgress.java
@@ -0,0 +1,13 @@
+package com.example.yannis.fractionne;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_SeanceInProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__seance_in_progress);
+    }
+}
