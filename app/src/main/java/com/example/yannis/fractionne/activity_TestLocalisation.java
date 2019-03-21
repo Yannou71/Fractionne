f67e02c258dbebcd1de75c431b50a62ad8030d30
yannou71 <yannis.banos@insa-lyon.fr>
2019-03-21 19:40:27 +0100

    Creation interface de Test

---
+++ app/src/main/java/com/example/yannis/fractionne/activity_TestLocalisation.java
@@ -0,0 +1,13 @@
+package com.example.yannis.fractionne;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_TestLocalisation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__test_localisation);
+    }
+}
