1c110f34843c2f214b03f2b93856fe1d05c05516
yannou71 <yannis.banos@insa-lyon.fr>
2019-03-22 23:04:04 +0100

    Test Localisation ok, récupération de long et lat

--- app/src/main/java/com/example/yannis/fractionne/activity_TestLocalisation.java
+++ app/src/main/java/com/example/yannis/fractionne/activity_TestLocalisation.java
@@ -6,17 +6,22 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+
 public class activity_TestLocalisation extends AppCompatActivity {
+
+
 
     private TextView tLocalisationX, tLocalisationY, tDistance, tTime;
     private final int updateTimeFrequency = 0;
     private final int updateDistanceFrequency = 0;
+    private final int MY_PERMISSIONS_REQUEST_LOCALISATION=314;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +36,6 @@ public class activity_TestLocalisation extends AppCompatActivity {
         tTime = findViewById(R.id.TestTime);
 
 
-
         //Création du Localisation Manager
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
@@ -39,9 +43,9 @@ public class activity_TestLocalisation extends AppCompatActivity {
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
-                tLocalisationX.setText(""+location.getLatitude());
-                tLocalisationY.setText(""+location.getLongitude());
-                tTime.setText("I'm update");
+                tLocalisationX.setText("" + location.getLatitude());
+                tLocalisationY.setText("" + location.getLongitude());
+
 
             }
 
@@ -59,26 +63,76 @@ public class activity_TestLocalisation extends AppCompatActivity {
 
         //On recupere la localistion contenue dans le Manager
         //Le if sert juste à ce que ça compile, pas besoin de comprendre
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+
+
+
+
+
+        tTime.setText("avant");
+
+        if (ContextCompat.checkSelfPermission(activity_TestLocalisation.this, Manifest.permission.ACCESS_FINE_LOCATION )!= PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+
         }
 
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(activity_TestLocalisation.this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity_TestLocalisation.this,Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(activity_TestLocalisation.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSIONS_REQUEST_LOCALISATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
+
         Location l = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-
+        
 
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCALISATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+
+
+
+
+
+
 
 
 
